7a597f12c8c2c47da7a871299df535d2c33ff371
undefined <65343244+NedoProgrammer@users.noreply.github.com>
2020-07-29 18:46:59 +0300

    First Version!

---
+++ MP4DurationEditor/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using LowLevelDesign.Hexify;
+using NExifTool;
+namespace MP4DurationEditor
+{
+    class Program
+    {
+        //The duration is located after the lmvhd part of the file, so, the program
+        //will search for that section.
+        private static byte[] LmvhdHex = { 0x6C, 0x6D, 0x76, 0x68, 0x64 };
+        private static void Main(string[] args)
+        {
+            Console.Write("Enter video path: ");
+            var videoPath = Console.ReadLine();
+            if(videoPath != null && !videoPath.EndsWith(".mp4")) throw new Exception("Only .mp4 videos are supported.");
+            try
+            {
+                Console.WriteLine("Getting video metadata..");
+                //Using exiftool to get the metadata of the file.
+                var exifTool =  new ExifTool(new ExifToolOptions(){IncludeBinaryTags = true});
+                var metadata = exifTool.GetTagsAsync(videoPath).Result;
+                var duration = metadata.First(x => x.Name == "Duration") ??
+                               throw new Exception("You entered a non-video file.");
+                var hexDuration = "";
+                if(duration.IsInteger)
+                    hexDuration = duration.GetInt64().ToString("x8");
+                else if (duration.IsDouble)
+                    hexDuration = ((long)(duration.GetDouble() * 1000)).ToString("x8");
+                Console.WriteLine("Converting video to hex..");
+                var hexString = Hex.ToHexString(File.ReadAllBytes(videoPath));
+                if (!hexString.Contains(Hex.ToHexString(LmvhdHex))) throw new Exception("The LMVHD part is not found. File is either unsupported, or corrupted.");
+                //Get all text after the lmvhd part
+                var lmvhdPart = hexString.Substring(hexString.IndexOf(Hex.ToHexString(LmvhdHex), StringComparison.Ordinal));
+                var newDuration = Prompt($"Enter new duration (hex, {hexDuration.Length} characters): ");
+                if(newDuration.Length != hexDuration.Length) throw new Exception("New duration's length is not equal to the original duration length.");
+                if(!long.TryParse(newDuration, NumberStyles.HexNumber, CultureInfo.CurrentCulture, out _)) throw new Exception("You entered a non-hex string.");
+                //Replace only the first occurence of the correct pattern (the duration)
+                Console.WriteLine("Replacing the correct bytes..");
+                var regex = new Regex(Regex.Escape(hexDuration));
+                var newText = regex.Replace(lmvhdPart, newDuration, 1);
+                Console.WriteLine("Writing result to the file..");
+                var newFilePath = Path.Combine(Path.GetDirectoryName(videoPath), Path.GetFileNameWithoutExtension(videoPath) + "_edited.mp4");
+                File.Create(newFilePath).Close();
+                //Replace the lmvhd part with the new, edited part
+                hexString = hexString.Replace(lmvhdPart, newText);
+                File.WriteAllBytes(newFilePath, Hex.FromHexString(hexString));
+				Console.WriteLine($"Done!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        //Same as input("prompt") in python
+        private static string Prompt(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine() ?? throw new ArgumentNullException();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# MP4DurationEditor 
+#### Edit the duration of MP4 files without corrupting them!
+A simple single-file C# script that edits bytes of MP4 files with the duration you want.
+
+**WARNING:** this program only edits the bytes of the file, it won't copy the content of the file to fit the duration.
+## Idea
+I was inspired by infinite discord videos, so i wanted to automate that.
+## Build
+To run this script, you'll need to build it.
+It uses .NET Core, which you can install here: https://dotnet.microsoft.com/download
+
+Just `cd` into the project directory (with the .csproj file) and enter: `dotnet build`
+
+When finished, you'll find the executable in `/bin/netcoreapp3.1/`
+## Dependencies
+ExifTool for getting video's metadata: https://exiftool.org/
+
+NExifTool - a wrapper for ExifTool: https://github.com/AerisG222/NExifTool
+
+Hexify for editing the hex of the mp4 file: https://github.com/lowleveldesign/hexify
+## Usage
+Simply run the executable, and follow the on-screen instructions :)
